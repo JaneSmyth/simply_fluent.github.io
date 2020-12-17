867323fffbc8912b6359645000c61b80372c06b4
Jane Smyth <43895552+JaneSmyth@users.noreply.github.com>
2020-12-17 19:05:16 +0000

    .

--- js/login.js
+++ js/login.js
@@ -63,32 +63,51 @@ function validateSignUp(){
   }
   validateUsernamePassword(username,password,creatAcIsValid)
 }
-function validateUsernamePassword(username,password,isValid) {
+
+function validateUsernamePassword(buttonClicked) {
+  var username = document.getElementById("loginEmail").value;
+  var password = document.getElementById("loginPassword").value;
+
   var userLength = username.length;
   var passLength = password.length;
 
-  if(isValid){
-    if(username == "" && password == ""){
-      alert("Please fill out all necessary fields.");
-      return false;
-    }
-    else if(userLength < 12 && passLength < 8){
-      alert("Username must be  12 characters or more. Password must be 8 characters or more.");
-      return false;
-    }
-    else if(username == "" || userLength < 12) {
-      alert("Your username email must have a minimum of 12 characters.");
-      return false;
-    }
-    else if(password = ""|| passLength < 8){
-      alert("Password must have 8 characters or more and contain at least one capital letter and at least one number.");
-      return false;
-    }
-    else{
-      return true;
-    }
+  if(username == "" && password == ""){
+    alert("Please fill out all necessary fields.");
+    return false;
+  }
+  else if(userLength < 12 && passLength < 8){
+    alert("Username must be  12 characters or more. Password must be 8 characters or more.");
+    return false;
+  }
+  else if(username == "" || userLength < 12) {
+    alert("Your username email must have a minimum of 12 characters.");
+    return false;
+  }
+  else if(password = ""|| passLength < 8){
+    alert("Password must have 8 characters or more and contain at least one capital letter and at least one number.");
+    return false;
+  }
+  else if(buttonClicked = signUpSubmit){
+      username = document.getElementById("signUpEmail").value;
+      password = document.getElementById("signUpPassword").value;
+
+      if(!fullNameValid){ //if user enters anything except letters (capital or lower) and spaces
+        alert("Your name can contain alphabet characters and spaces only.");
+        return false;
+      }
+      else if(!fullName){
+        alert("Please fill out all necessary fields.");
+        return false;
+      }
+      else if(confirmPassSame==false){ //pass and confirm pass not same
+        alert("Passwords do not match. Please try again.");
+        return false;
+      }
+      else{
+        return true;
+      }
   }
   else{
-    return false;
+    return true;
   }
 }
