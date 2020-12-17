28e45f8ec15b753a87649b6cfab30cdebc1eab9a
Jane Smyth <43895552+JaneSmyth@users.noreply.github.com>
2020-12-17 19:23:44 +0000

    .

--- js/login.js
+++ js/login.js
@@ -35,52 +35,62 @@ $(document).ready(function(){
 
 
 function validateUsernamePassword(buttonClicked) {
-  var username = document.getElementById("loginEmail").value;
-  var password = document.getElementById("loginPassword").value;
+  var username;
+  var password;
 
   var userLength = username.length;
   var passLength = password.length;
+  var flag = "continue";
 
-  if(username == "" && password == ""){
-    alert("Please fill out all necessary fields.");
-    return false;
-  }
-  else if(userLength < 12 && passLength < 8){
-    alert("Username must be  12 characters or more. Password must be 8 characters or more.");
-    return false;
-  }
-  else if(username == "" || userLength < 12) {
-    alert("Your username email must have a minimum of 12 characters.");
-    return false;
-  }
-  else if(password = ""|| passLength < 8){
-    alert("Password must have 8 characters or more and contain at least one capital letter and at least one number.");
-    return false;
-  }
-  else {
-      if(buttonClicked == "signUpSubmit"){
-        username = document.getElementById("signUpEmail").value;
-        password = document.getElementById("signUpPassword").value;
+  if(buttonClicked == "signUpSubmit"){
+      username = document.getElementById("signUpEmail").value;
+      password = document.getElementById("signUpPassword").value;
 
-        if(!fullNameValid){ //if user enters anything except letters (capital or lower) and spaces
-          alert("Your name can contain alphabet characters and spaces only.");
-          return false;
-        }
-        else if(!fullName){
-          alert("Please fill out all necessary fields.");
-          return false;
-        }
-        else if(!confirmPassSame){ //pass and confirm pass not same
-          alert("Passwords do not match. Please try again.");
-          return false;
-        }
-        else{
-          return true;
-        }
+      if(!fullNameValid){ //if user enters anything except letters (capital or lower) and spaces
+        alert("Your name can contain alphabet characters and spaces only.");
+        flag = "no";
+        return false;
+      }
+      else if(!fullName){
+        alert("Please fill out all necessary fields.");
+        flag = "no";
+        return false;
+      }
+      else if(!confirmPassSame){ //pass and confirm pass not same
+        alert("Passwords do not match. Please try again.");
+        flag="no";
+        return false;
+      }
+      else{
+        flag="continue";
+      }
+    }
+    if(flag="continue"){
+      username = document.getElementById("loginEmail").value;
+      password = document.getElementById("loginPassword").value;
+      
+      if(username == "" && password == ""){
+        alert("Please fill out all necessary fields.");
+        return false;
+      }
+      else if(userLength < 12 && passLength < 8){
+        alert("Username must be  12 characters or more. Password must be 8 characters or more.");
+        return false;
+      }
+      else if(username == "" || userLength < 12) {
+        alert("Your username email must have a minimum of 12 characters.");
+        return false;
+      }
+      else if(password = ""|| passLength < 8){
+        alert("Password must have 8 characters or more and contain at least one capital letter and at least one number.");
+        return false;
       }
       else{
         return true;
       }
-  }
+    }
+    else{
+      return false;
+    }
 
-}
+  }
